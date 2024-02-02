2139b0ad5dcdf246db43606a063ac24ce32a6012
solowey <solowey1@gmail.com>
2024-02-02 03:08:02 +0300

    Comment default Telegram theme settings

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -39,20 +39,20 @@ export function useTelegram() {
 
     const defaultSettings = () => {
         tg.colorScheme = 'dark';
-        const theme = tg.themeParams;
-        theme.bg_color = '#0E2133';
-        theme.text_color = '#FFFFFF';
-        theme.hint_color = '#93ADC5';
-        theme.link_color = '#47A7FF';
-        theme.button_color = '#47A7FF';
-        theme.button_text_color = '#FFFFFF';
-        theme.secondary_bg_color = '#1C4366';
-        theme.header_bg_color = '#0E2133';
-        theme.accent_text_color = '#91CAFF';
-        theme.section_bg_color = '#0E2133';
-        theme.section_header_text_color = '#0E2133';
-        theme.subtitle_text_color = '#93ADC5';
-        theme.destructive_text_color = '#992B64';
+        // const theme = tg.themeParams;
+        // theme.bg_color = '#0E2133';
+        // theme.text_color = '#FFFFFF';
+        // theme.hint_color = '#93ADC5';
+        // theme.link_color = '#47A7FF';
+        // theme.button_color = '#47A7FF';
+        // theme.button_text_color = '#FFFFFF';
+        // theme.secondary_bg_color = '#1C4366';
+        // theme.header_bg_color = '#0E2133';
+        // theme.accent_text_color = '#91CAFF';
+        // theme.section_bg_color = '#0E2133';
+        // theme.section_header_text_color = '#0E2133';
+        // theme.subtitle_text_color = '#93ADC5';
+        // theme.destructive_text_color = '#992B64';
         settingsButton();
     }
 
