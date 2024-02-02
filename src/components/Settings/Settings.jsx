c0b18dede6d1ca85b60a79222c99655b0f90dd3b
solowey <solowey1@gmail.com>
2024-02-02 03:26:48 +0300

    Test Telegram settings button

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -3,9 +3,18 @@ import { useTelegram } from '../../hooks/useTelegram';
 
 
 const Settings = (props) => {
-    const {showBackButton, user} = useTelegram();
+    const { showBackButton, user } = useTelegram();
     showBackButton();
-    
+
+    window.Telegram.WebApp.SettingsButton.onClick(() => {
+        let navigate = useNavigate();
+        navigate('settings');
+    });
+    window.Telegram.WebApp.onEvent('settingsButtonClicked', () => {
+        let navigate = useNavigate();
+        navigate('settings');
+    });
+
     return (
         <div className='settings'>
             <h2>Настройки</h2>
