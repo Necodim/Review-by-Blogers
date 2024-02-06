45c807e00337cf819299258b1c90d9d6f719900f
solowey <solowey1@gmail.com>
2024-02-06 15:05:06 +0300

    Deploy for yooKassa 3

--- src/hooks/getProfile.js
+++ src/hooks/getProfile.js
@@ -1,7 +1,7 @@
 export function getProfile() {
     const userType = 'seller';
     const onboarding = false;
-    const subscription = true;
+    const subscription = false;
     const subscriptionExpiration = new Date(2025, 1, 1);
     const api = true;
     const trial = false;
