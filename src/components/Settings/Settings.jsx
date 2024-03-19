5fa4319dbb6d7ce84a62012c28e1b7aa80f3001c
solowey <solowey1@gmail.com>
2024-03-19 22:25:57 +0300

    Update Settings.jsx

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTelegram } from '../../hooks/useTelegram';
+import Link from '../Button/Link';
 
 const Settings = (props) => {
     const navigate = useNavigate();
@@ -24,12 +25,26 @@ const Settings = (props) => {
         });
     }
 
+    const goToStartScreen = () => {
+        navigate('/');
+    }
+
     return (
-        <div className='settings'>
-            <h2>Настройки</h2>
-            <span className={'username'}>
-                {user?.username}
-            </span>
+        <div className='content-wrapper'>
+            <Header />
+            <div className='container' id='settings' >
+                <div className='list'>
+                    <div className='list-item'>
+                        <h2>Настройки</h2>
+                    </div>
+                    <div className='list-item'>
+                        <span className={'username'}>{user?.username}</span>
+                    </div>
+                    <div className='list-item'>
+                        <Link onClick={goToStartScreen}>StartScreen</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
