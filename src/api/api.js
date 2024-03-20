a5d869c9afe19c3d0ad83ac44ec62b8d149e770f
solowey <solowey1@gmail.com>
2024-03-20 20:06:34 +0300

    Change base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL
+    baseURL: 'https://reviewbybloggers.ru/api'
+    // baseURL: process.env.REACT_APP_BACKEND_URL
 });
 
 apiClient.interceptors.request.use(config => {
