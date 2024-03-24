fd446fd1bbd562cd27879fbcf33ebb211289fcd0
solowey <solowey1@gmail.com>
2024-03-25 02:40:30 +0300

    Old version of toast

--- src/hooks/useToast.js
+++ src/hooks/useToast.js
@@ -1,10 +1,11 @@
 import { useEffect, useRef } from 'react';
 import { toast, Slide } from 'react-toastify';
-import '../components/Toast/Toast.css';
+import '../components/Toast/Toast.css'
 import { useTelegram } from './useTelegram';
 
 export const useToastManager = () => {
   const loadingToastIdRef = useRef(null);
+
   const { tg } = useTelegram();
 
   const showToast = (message, type) => {
@@ -20,38 +21,36 @@ export const useToastManager = () => {
     };
 
     if (type === 'loading') {
+      // Показываем или обновляем тост загрузки
       if (loadingToastIdRef.current === null) {
         loadingToastIdRef.current = toast.loading(message, options);
       } else {
         toast.update(loadingToastIdRef.current, { ...options, render: message, isLoading: true });
       }
     } else {
+      // Закрываем тост загрузки, если он открыт
       if (loadingToastIdRef.current !== null) {
         toast.dismiss(loadingToastIdRef.current);
-        loadingToastIdRef.current = null;
+        loadingToastIdRef.current = null; // Сброс ID тоста загрузки
       }
+      if (type === 'success') {
+        toast.success(message, options);
+        tg.notificationOccurred(type);
+      } else if (type === 'error') {
+        toast.error(message, options);
+        tg.notificationOccurred(type);
 
-      switch (type) {
-        case 'success':
-          toast.success(message, options);
-          tg.notificationOccurred(type);
-          break;
-        case 'error':
-          toast.error(message, options);
-          tg.notificationOccurred(type);
-          break;
-        case 'warning':
-          toast.warning(message, options);
-          tg.notificationOccurred(type);
-          break;
-        default:
-          toast.info(message, options);
-          tg.notificationOccurred('success');
-          break;
+      } else if (type === 'warning') {
+        toast.warning(message, options);
+        tg.notificationOccurred(type);
+      } else {
+        toast.info(message, options);
+        tg.notificationOccurred('success');
       }
     }
   };
 
+  // Предоставляем функцию для сброса ID тоста загрузки, если нужно закрыть его вручную
   const resetLoadingToast = () => {
     if (loadingToastIdRef.current !== null) {
       toast.dismiss(loadingToastIdRef.current);
