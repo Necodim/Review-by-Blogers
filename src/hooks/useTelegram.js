9fea39e4b7526d41641bacea9155e99980ebd56a
solowey <solowey1@gmail.com>
2024-03-25 02:37:36 +0300

    popup bottom fix and theme tg

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -75,22 +75,22 @@ export function useTelegram() {
     }
 
     const defaultSettings = () => {
-        const theme = tg.themeParams;
-        theme.bg_color = '#1C4366';
+        // const theme = tg.themeParams;
+        // theme.bg_color = '#1C4366';
         // theme.text_color = '#FFFFFF';
         // theme.hint_color = '#93ADC5';
         // theme.link_color = '#FFFFFF';
         // theme.button_color = '#47A7FF';
         // theme.button_text_color = '#FFFFFF';
         // theme.secondary_bg_color = '#1C4366';
-        theme.header_bg_color = '#0E2133';
+        // theme.header_bg_color = '#0E2133';
         // theme.accent_text_color = '#91CAFF';
         // theme.section_bg_color = '#0E2133';
         // theme.section_header_text_color = '#0E2133';
         // theme.subtitle_text_color = '#93ADC5';
-        theme.destructive_text_color = '#FF474B';
-        // tg.setHeaderColor('#0E2133');
-        // tg.setBackgroundColor('#1C4366');
+        // theme.destructive_text_color = '#FF474B';
+        tg.setHeaderColor('#0E2133');
+        tg.setBackgroundColor('#1C4366');
         tg.enableClosingConfirmation();
         tg.expand();
         BackButton();
