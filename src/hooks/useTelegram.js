f5bb5352aee08cdeff508290bb1ef7909a02de22
solowey <solowey1@gmail.com>
2024-03-25 02:29:39 +0300

    Update theme params

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -75,7 +75,7 @@ export function useTelegram() {
     }
 
     const defaultSettings = () => {
-        // const theme = tg.themeParams;
+        const theme = tg.themeParams;
         theme.bg_color = '#1C4366';
         // theme.text_color = '#FFFFFF';
         // theme.hint_color = '#93ADC5';
@@ -83,15 +83,14 @@ export function useTelegram() {
         // theme.button_color = '#47A7FF';
         // theme.button_text_color = '#FFFFFF';
         // theme.secondary_bg_color = '#1C4366';
-        theme.header_bg_color = '#0E2133';
+        // theme.header_bg_color = '#0E2133';
         // theme.accent_text_color = '#91CAFF';
         // theme.section_bg_color = '#0E2133';
         // theme.section_header_text_color = '#0E2133';
         // theme.subtitle_text_color = '#93ADC5';
-        theme.destructive_text_color = '#ff474b';
+        theme.destructive_text_color = 'var(--error)';
         // tg.setHeaderColor('#0E2133');
-        // tg.setBackgroundColor('#0E2133');
-        // tg.setBackgroundColor('#1C4366');
+        tg.setBackgroundColor('#1C4366');
         tg.enableClosingConfirmation();
         tg.expand();
         BackButton();
