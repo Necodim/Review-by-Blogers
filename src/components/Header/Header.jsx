f1e75fe844ab52bf7a30d8f026fd6dcb28ccb5f3
solowey <solowey1@gmail.com>
2024-04-22 21:13:47 +0300

    update Header and UserAgreementPage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,66 +1,40 @@
 import React from 'react'
 import './Header.css'
-import { Link, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useUserProfile } from '../../hooks/UserProfileContext';
 import Button from '../../components/Button/Button'
 import Icon from '../Icon/Icon'
 
 const Header = (props) => {
-    const { profile, loading } = useUserProfile();
-    const location = useLocation();
+  const { profile, loading } = useUserProfile();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const isActive = (path) => {
-        // const paths = location.pathname.split('/');
-        // const result = paths[paths.length - 1] === path ? 'active' : '';
-        const result = location.pathname.indexOf(path) !== -1 ? 'active' : '';
-        return result;
-    };
+  const isActive = (path) => {
+    // const paths = location.pathname.split('/');
+    // const result = paths[paths.length - 1] === path ? 'active' : '';
+    const result = location.pathname.indexOf(path) !== -1 ? 'active' : '';
+    return result;
+  };
 
-    if (loading) {
-        return (
-            <div className={'header'}>
-                <div className="nav-buttons-wrapper">
-                    <Button className={`nav-button light ${isActive('/store') ? 'active' : ''}`}>
-                        <Icon icon='grid_view' />
-                        <span>{profile.role === 'blogger' ? 'Категории' : 'Товары'}</span>
-                    </Button>
-                    <Button className={`nav-button light ${isActive('/barters') ? 'active' : ''}`}>
-                        <Icon icon='groups' />
-                        <span>Бартеры</span>
-                    </Button>
-                    <Button className={`nav-button light ${isActive('/profile') ? 'active' : ''}`}>
-                        <Icon icon='person' />
-                        <span>Профиль</span>
-                    </Button>
-                </div>
-            </div>
-        )
-    }
-
-    return (
-        <div className={'header'}>
-            <div className="nav-buttons-wrapper">
-                <Link to={ `../store` }>
-                    <Button className={`nav-button light ${isActive('/store') ? 'active' : ''}`}>
-                        <Icon icon='grid_view' />
-                        <span>{profile.role === 'blogger' ? 'Категории' : 'Товары'}</span>
-                    </Button>
-                </Link>
-                <Link to={ `../barters` }>
-                    <Button className={`nav-button light ${isActive('/barters') ? 'active' : ''}`}>
-                        <Icon icon='groups' />
-                        <span>Бартеры</span>
-                    </Button>
-                </Link>
-                <Link to={ `../profile` }>
-                    <Button className={`nav-button light ${isActive('/profile') ? 'active' : ''}`}>
-                        <Icon icon='person' />
-                        <span>Профиль</span>
-                    </Button>
-                </Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className={'header'}>
+      <div className="nav-buttons-wrapper">
+        <Button className={`nav-button light ${isActive('/store') ? 'active' : ''}`} onClick={loading ? navigate('/store') : () => {}}>
+          <Icon icon='grid_view' />
+          <span>{profile.role === 'blogger' ? 'Категории' : 'Товары'}</span>
+        </Button>
+        <Button className={`nav-button light ${isActive('/barters') ? 'active' : ''}`} onClick={loading ? navigate('/barters') : () => {}}>
+          <Icon icon='groups' />
+          <span>Бартеры</span>
+        </Button>
+        <Button className={`nav-button light ${isActive('/profile') ? 'active' : ''}`} onClick={loading ? navigate('/profile') : () => {}}>
+          <Icon icon='person' />
+          <span>Профиль</span>
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default Header
