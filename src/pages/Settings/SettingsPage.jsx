4f79263d4851a548f73c318bf14f8e5823e63fb0
solowey <solowey1@gmail.com>
2024-06-04 21:54:48 +0300

    upd serttings page

--- src/pages/Settings/SettingsPage.jsx
+++ src/pages/Settings/SettingsPage.jsx
@@ -9,100 +9,106 @@ import Link from '../../components/Button/Link';
 import PopupConfirmation from '../../components/Popup/PopupConfirmation';
 
 const SettingsPage = () => {
-    const navigate = useNavigate();
-    const { profile, updateProfile, updateUserData, addSubscription } = useUserProfile();
-    const { isAvailable, showBackButton, user } = useTelegram();
-    const { showToast } = useToastManager();
+	const navigate = useNavigate();
+	const { profile, updateProfile, updateUserData, addSubscription } = useUserProfile();
+	const { isAvailable, showBackButton, user } = useTelegram();
+	const { showToast } = useToastManager();
 
-    const [isPopupChangeRoleVisible, setIsPopupChangeRoleVisible] = useState(false);
-    const [currentRole, setCurrentRole] = useState(profile.role);
-    const [newRole, setNewRole] = useState(profile.role);
+	const [isPopupChangeRoleVisible, setIsPopupChangeRoleVisible] = useState(false);
+	const [currentRole, setCurrentRole] = useState(profile.role);
+	const [newRole, setNewRole] = useState(profile.role);
 
-    useEffect(() => {
-        if (isAvailable) showBackButton();
-    }, [isAvailable, showBackButton]);
+	useEffect(() => {
+		if (isAvailable) showBackButton();
+	}, [isAvailable, showBackButton]);
 
-    useEffect(() => {
-        setCurrentRole(profile.role);
-        setNewRole(profile.role);
-    }, [profile.role]);
+	useEffect(() => {
+		setCurrentRole(profile.role);
+		setNewRole(profile.role);
+	}, [profile.role]);
 
-    // // Тестовый функционал
-    // const goToStartScreen = () => {
-    //     navigate('/');
-    // }
+	// // Тестовый функционал
+	// const goToStartScreen = () => {
+	//     navigate('/');
+	// }
 
-    // // Тестовый функционал
-    // const addSellerSubscription = async () => {
-    //     const data = {
-    //         yookassa_id: '39afc4a2-6325-475b-9980-c4323ed72fa6',
-    //         amount_value: 10000,
-    //         amount_currency: 'RUB'
-    //     }
-    //     const addedSubscription = await addSubscription(data);
-    //     updateProfile({ subscription: addedSubscription });
-    //     goToStartScreen();
-    // }
+	// // Тестовый функционал
+	// const addSellerSubscription = async () => {
+	//     const data = {
+	//         yookassa_id: '39afc4a2-6325-475b-9980-c4323ed72fa6',
+	//         amount_value: 10000,
+	//         amount_currency: 'RUB'
+	//     }
+	//     const addedSubscription = await addSubscription(data);
+	//     updateProfile({ subscription: addedSubscription });
+	//     goToStartScreen();
+	// }
 
-    // // Тестовый функционал
-    // const removeRole = async () => {
-    //     await updateUserData({role: ''});
-    //     setCurrentRole(newRole);
-    //     updateProfile({ role: newRole });
-    //     goToStartScreen();
-    // }
+	// // Тестовый функционал
+	// const removeRole = async () => {
+	//     await updateUserData({role: ''});
+	//     setCurrentRole(newRole);
+	//     updateProfile({ role: newRole });
+	//     goToStartScreen();
+	// }
 
-    const radioOptions = [
-        { label: 'Селлер', value: 'seller', icon: 'store' },
-        { label: 'Блогер', value: 'blogger', icon: 'face_retouching_natural' },
-    ];
+	const radioOptions = [
+		{ label: 'Селлер', value: 'seller', icon: 'store' },
+		{ label: 'Блогер', value: 'blogger', icon: 'face_retouching_natural' },
+	];
 
-    const handleRadioChange = (value) => {
-        if (profile.role !== value) {
-            setNewRole(value);
-            setIsPopupChangeRoleVisible(true);
-        }
-    }
+	const handleRadioChange = (value) => {
+		if (profile.role !== value) {
+			setNewRole(value);
+			setIsPopupChangeRoleVisible(true);
+		}
+	}
 
-    const closePopupWithoutChangeRole = () => {
-        showToast('Роль не изменилась');
-        setNewRole(currentRole);
-        setIsPopupChangeRoleVisible(false);
-    }
+	const closePopupWithoutChangeRole = () => {
+		showToast('Роль не изменилась');
+		setNewRole(currentRole);
+		setIsPopupChangeRoleVisible(false);
+	}
 
-    const changeRole = async () => {
-        try {
-            await updateUserData({ role: newRole });
-            updateProfile({ role: newRole });
-            goToStartScreen();
-        } catch (error) {
-            showToast('Ошибка при обновлении роли', 'error');
-        }
-    } 
+	const changeRole = async () => {
+		try {
+			await updateUserData({ role: newRole });
+			updateProfile({ role: newRole });
+			goToStartScreen();
+		} catch (error) {
+			showToast('Ошибка при обновлении роли', 'error');
+		}
+	}
 
-    return (
-        <div className='content-wrapper'>
-            <div className='container' id='settings'>
-                <h2>Настройки</h2>
-                <div>{'Ваш ID: ' + profile.id}</div>
-                <div>{'Ваш username: ' + profile.username}</div>
-                {profile && profile.role &&
-                <Input
-                    id='settings-role'
-                    title='Роль'
-                    type='radio'
-                    options={radioOptions}
-                    onChange={handleRadioChange}
-                    selectedValue={newRole}
-                />
-                }
-                <Button onClick={() => {navigate('/info/support')}} icon='support_agent'>Поддержка</Button>
-                <Link onClick={() => {navigate('/info/user-agreement')}}>Пользовательское соглашение</Link>
-            </div>
+	return (
+		<div className='content-wrapper'>
+			<div className='container' id='settings'>
+				<div className='list'>
+					<div className='list-item'>
+
+						<h2>Настройки</h2>
+					</div>
+					<div className='list-item'>
+						<small>{'Ваш ID: ' + profile.id}</small>
+					</div>
+				</div>
+				{profile && profile.role &&
+					<Input
+						id='settings-role'
+						title='Роль'
+						type='radio'
+						options={radioOptions}
+						onChange={handleRadioChange}
+						selectedValue={newRole}
+					/>
+				}
+				<Button onClick={() => { navigate('/info/support') }} icon='support_agent'>Поддержка</Button>
+				<Link onClick={() => { navigate('/info/user-agreement') }}>Пользовательское соглашение</Link>
+			</div>
 
 
-            
-            {/* <div className='container' id='tests'>
+
+			{/* <div className='container' id='tests'>
                 <h2>Тестовый функционал</h2>
                 <span>Telergam username: {user?.username}</span>
                 <Link onClick={goToStartScreen}>Стартовый экран</Link>
@@ -111,18 +117,18 @@ const SettingsPage = () => {
                     <Link onClick={addSellerSubscription}>Включить подписку (бесплатно)</Link>
                 }
             </div> */}
-            <PopupConfirmation
-                id='popup-change-role-confirmation'
-                title='Изменение роли'
-                text='Вы действительно хотите изменить роль?'
-                descr='После смены роли вы не потеряете свои данные. Её можно будет точно так же изменить в будущем через настройки.'
-                isOpen={isPopupChangeRoleVisible}
-                onClose={closePopupWithoutChangeRole}
-                onConfirmation={changeRole}
-                timer={4}
-            />
-        </div>
-    )
+			<PopupConfirmation
+				id='popup-change-role-confirmation'
+				title='Изменение роли'
+				text='Вы действительно хотите изменить роль?'
+				descr='После смены роли вы не потеряете свои данные. Её можно будет точно так же изменить в будущем через настройки.'
+				isOpen={isPopupChangeRoleVisible}
+				onClose={closePopupWithoutChangeRole}
+				onConfirmation={changeRole}
+				timer={4}
+			/>
+		</div>
+	)
 }
 
 export default SettingsPage;
