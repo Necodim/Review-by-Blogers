134531c02c95195cc68fafac1af2ed01c3af5be5
albertincx <albertincx@gmail.com>
2024-06-12 11:57:44 +0700

    avoid same navigate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,34 +5,29 @@ import { useUserProfile } from '../../hooks/UserProfileContext';
 import Button from '../../components/Button/Button'
 import Icon from '../Icon/Icon'
 
-const Header = (props) => {
-  const { profile, loading } = useUserProfile();
+const Header = () => {
+  const { profile } = useUserProfile();
   const navigate = useNavigate();
   const location = useLocation();
 
-  const isActive = (path) => {
-    // const paths = location.pathname.split('/');
-    // const result = paths[paths.length - 1] === path ? 'active' : '';
-    const result = location.pathname.indexOf(path) !== -1 ? 'active' : '';
-    return result;
-  };
+  const isActive = (path) => location.pathname === path;
 
-  const navigateTo = (path) => {
-    navigate(path);
+  const navigateTo = (path) => () => {
+    if (!isActive(path)) navigate(path);
   }
 
   return (
     <div className={'header'}>
       <div className="nav-buttons-wrapper">
-        <Button className={`nav-button light ${isActive('/store') ? 'active' : ''}`} onClick={() => navigateTo('/store')}>
+        <Button className={`nav-button light ${isActive('/store') ? 'active' : ''}`} onClick={navigateTo('/store')}>
           <Icon icon='grid_view' />
           <span>{profile.role === 'blogger' ? 'Категории' : 'Товары'}</span>
         </Button>
-        <Button className={`nav-button light ${isActive('/barters') ? 'active' : ''}`} onClick={() => navigateTo('/barters')}>
+        <Button className={`nav-button light ${isActive('/barters') ? 'active' : ''}`} onClick={navigateTo('/barters')}>
           <Icon icon='groups' />
           <span>Бартеры</span>
         </Button>
-        <Button className={`nav-button light ${isActive('/profile') ? 'active' : ''}`} onClick={() => navigateTo('/profile')}>
+        <Button className={`nav-button light ${isActive('/profile') ? 'active' : ''}`} onClick={navigateTo('/profile')}>
           <Icon icon='person' />
           <span>Профиль</span>
         </Button>
