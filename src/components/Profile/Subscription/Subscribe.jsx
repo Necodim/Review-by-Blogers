c231627406a377cbe7de61fda5722f019f93fa7d
solowey <solowey1@gmail.com>
2024-06-15 17:22:08 +0300

    upd subscription

--- src/components/Profile/Subscription/Subscribe.jsx
+++ src/components/Profile/Subscription/Subscribe.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { useUserProfile } from '../../../hooks/UserProfileContext';
 import SubscribeHasSubscription from './SubscribeHasSubscription';
 import SubscribeHasNoSubscription from './SubscribeHasNoSubscription';
 
 const Subscribe = () => {
-    const { period } = location.state || {};
+    const location = useLocation();
+    const { period } = location.state || { period: 'month' };
     const { profile } = useUserProfile();
 
-    return profile.subscription?.active ? <SubscribeHasSubscription /> : <SubscribeHasNoSubscription period={period.period} />;
+    return profile.subscription?.active ? <SubscribeHasSubscription /> : <SubscribeHasNoSubscription period={period} />;
 }
 
 export default Subscribe;
