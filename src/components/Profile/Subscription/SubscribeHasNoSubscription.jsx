90fe305ec8965f951be967b3bc6040c81c2e4632
solowey <solowey1@gmail.com>
2024-06-16 23:35:43 +0300

    upd

--- src/components/Profile/Subscription/SubscribeHasNoSubscription.jsx
+++ src/components/Profile/Subscription/SubscribeHasNoSubscription.jsx
@@ -77,10 +77,10 @@ const SubscribeHasNoSubscription = ({ period }) => {
       };
   
       const prepareWidget = async () => {
+        setLoadingText('');
         const params = getParams(token);
   
         script.onload = () => {
-          setLoadingText('');
           const checkout = new window.YooMoneyCheckoutWidget(params);
           document.getElementById('payment-form').innerHTML = '';
           checkout.render('payment-form');
@@ -88,7 +88,7 @@ const SubscribeHasNoSubscription = ({ period }) => {
   
         document.body.appendChild(script);
       };
-  
+      
       prepareWidget();
   
       return () => {
@@ -107,149 +107,150 @@ const SubscribeHasNoSubscription = ({ period }) => {
     }
   }, [errorMessage, attemptedSubmit, showToast]);
 
-  useEffect(() => {
-    const keys = Object.keys(formData);
-    let array = new Array();
-    for (let field of keys) {
-      if (formData[field].trim() === '') array.push(true);
-    }
-    const isValid = array.length === 0;
-    setIsFormValid(isValid);
-  }, [formData]);
+  // useEffect(() => {
+  //   const keys = Object.keys(formData);
+  //   let array = new Array();
+  //   for (let field of keys) {
+  //     if (formData[field].trim() === '') array.push(true);
+  //   }
+  //   const isValid = array.length === 0;
+  //   setIsFormValid(isValid);
+  // }, [formData]);
 
-  const addFormData = (name, value) => {
-    setFormData(prev => ({ ...prev, [name]: value }));
-  }
+  // const addFormData = (name, value) => {
+  //   setFormData(prev => ({ ...prev, [name]: value }));
+  // }
 
-  const handleCardNumberChange = (event) => {
-    setAttemptedSubmit(false);
-    let { name, value } = event.target;
-    value = value.replace(/\D+/g, '');
-    value = value.replace(/(\d{4})(?=\d)/g, '$1 ');
-    setCardNumberValue(value);
-    addFormData(name, value);
-  }
+  // const handleCardNumberChange = (event) => {
+  //   setAttemptedSubmit(false);
+  //   let { name, value } = event.target;
+  //   value = value.replace(/\D+/g, '');
+  //   value = value.replace(/(\d{4})(?=\d)/g, '$1 ');
+  //   setCardNumberValue(value);
+  //   addFormData(name, value);
+  // }
 
-  const handleCardPeriodChange = (event) => {
-    setAttemptedSubmit(false);
-    let { name, value } = event.target;
-    value = value.replace(/\D+/g, '');
-    value = value.replace(/(\d{2})(?=\d)/g, '$1/');
-    setCardPeriodValue(value);
-    addFormData(name, value);
-  }
+  // const handleCardPeriodChange = (event) => {
+  //   setAttemptedSubmit(false);
+  //   let { name, value } = event.target;
+  //   value = value.replace(/\D+/g, '');
+  //   value = value.replace(/(\d{2})(?=\d)/g, '$1/');
+  //   setCardPeriodValue(value);
+  //   addFormData(name, value);
+  // }
 
-  const handleCardCodeChange = (event) => {
-    setAttemptedSubmit(false);
-    let { name, value } = event.target;
-    value = value.replace(/\D+/g, '');
-    setCardCodeValue(value);
-    addFormData(name, value);
-  }
+  // const handleCardCodeChange = (event) => {
+  //   setAttemptedSubmit(false);
+  //   let { name, value } = event.target;
+  //   value = value.replace(/\D+/g, '');
+  //   setCardCodeValue(value);
+  //   addFormData(name, value);
+  // }
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setAttemptedSubmit(true);
-    setIsFormValid(false);
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   setAttemptedSubmit(true);
+  //   setIsFormValid(false);
 
-    if (!cardNumberValue || !cardPeriodValue || !cardCodeValue) {
-      setErrorMessage('Все поля должны быть заполнены.');
-      return;
-    }
+  //   if (!cardNumberValue || !cardPeriodValue || !cardCodeValue) {
+  //     setErrorMessage('Все поля должны быть заполнены.');
+  //     return;
+  //   }
 
-    const periodParts = cardPeriodValue.split('/');
-    if (periodParts.length !== 2) {
-      setErrorMessage('Неверный формат срока действия карты');
-      return;
-    }
+  //   const periodParts = cardPeriodValue.split('/');
+  //   if (periodParts.length !== 2) {
+  //     setErrorMessage('Неверный формат срока действия карты');
+  //     return;
+  //   }
 
-    const month = parseInt(periodParts[0], 10);
-    const year = parseInt(periodParts[1], 10) + 2000;
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth() + 1;
+  //   const month = parseInt(periodParts[0], 10);
+  //   const year = parseInt(periodParts[1], 10) + 2000;
+  //   const currentDate = new Date();
+  //   const currentYear = currentDate.getFullYear();
+  //   const currentMonth = currentDate.getMonth() + 1;
 
-    if (month < 1 || month > 12) {
-      setErrorMessage('Месяц должен быть в диапазоне от 01 до 12');
-      return;
-    }
+  //   if (month < 1 || month > 12) {
+  //     setErrorMessage('Месяц должен быть в диапазоне от 01 до 12');
+  //     return;
+  //   }
 
-    if (year < currentYear || (year === currentYear && month < currentMonth)) {
-      setErrorMessage('Срок действия карты истёк, попробуйте использовать другую карту');
-      return;
-    }
+  //   if (year < currentYear || (year === currentYear && month < currentMonth)) {
+  //     setErrorMessage('Срок действия карты истёк, попробуйте использовать другую карту');
+  //     return;
+  //   }
 
-    const cardData = {
-      number: cardNumberValue.replace(/\D/gi, ''),
-      month: periodParts[0],
-      year: periodParts[1],
-      cvc: cardCodeValue
-    }
+  //   const cardData = {
+  //     number: cardNumberValue.replace(/\D/gi, ''),
+  //     month: periodParts[0],
+  //     year: periodParts[1],
+  //     cvc: cardCodeValue
+  //   }
 
-    try {
-      const payment = await createYookassaPayload(cardData);
-    } catch (error) {
-      setIsFormValid(true);
-      console.log(error);
-    }
-  }
+  //   try {
+  //     const payment = await createYookassaPayload(cardData);
+  //   } catch (error) {
+  //     setIsFormValid(true);
+  //     console.log(error);
+  //   }
+  // }
 
   if (!!loadingText) {
     return <PreloaderContainer text={loadingText} />
+  } else {
+    return (
+      <div className='content-wrapper'>
+        <Header />
+        {/* <div className='container' id='subscribe'>
+          <div className='list'>
+            <div className='list-item'>
+              <h2>Оформление подписки</h2>
+            </div>
+            <div className='list-item justify-content-start'>
+              <small>Стоимость:</small>
+              <span>10 000 ₽</span>
+            </div>
+          </div>
+          <Form className='form-wrapper' btnicon='wallet' btntext='Оформить подписку' isDisabled={!isFormValid} onSubmit={handleSubmit}>
+            <Input
+              id='card-number'
+              name='card-number'
+              title='Номер карты'
+              placeholder='2202 2020 1234 5678'
+              required='required'
+              autoComplete='cc-number'
+              value={cardNumberValue}
+              onChange={handleCardNumberChange}
+              maxLength='19'
+            />
+            <Input
+              id='card-period'
+              name='card-period'
+              title='Действительна до'
+              placeholder='09/32'
+              required='required'
+              autoComplete='cc-exp'
+              value={cardPeriodValue}
+              onChange={handleCardPeriodChange}
+              maxLength='5'
+            />
+            <Input
+              id='card-code'
+              name='card-code'
+              title='CVC/CVV код'
+              placeholder='123'
+              required='required'
+              autoComplete='cc-csc'
+              value={cardCodeValue}
+              onChange={handleCardCodeChange}
+              maxLength='3'
+            />
+          </Form>
+        </div> */}
+        <div id="payment-form"></div>
+      </div>
+    )
   }
 
-  return (
-    <div className='content-wrapper'>
-      <Header />
-      {/* <div className='container' id='subscribe'>
-        <div className='list'>
-          <div className='list-item'>
-            <h2>Оформление подписки</h2>
-          </div>
-          <div className='list-item justify-content-start'>
-            <small>Стоимость:</small>
-            <span>10 000 ₽</span>
-          </div>
-        </div>
-        <Form className='form-wrapper' btnicon='wallet' btntext='Оформить подписку' isDisabled={!isFormValid} onSubmit={handleSubmit}>
-          <Input
-            id='card-number'
-            name='card-number'
-            title='Номер карты'
-            placeholder='2202 2020 1234 5678'
-            required='required'
-            autoComplete='cc-number'
-            value={cardNumberValue}
-            onChange={handleCardNumberChange}
-            maxLength='19'
-          />
-          <Input
-            id='card-period'
-            name='card-period'
-            title='Действительна до'
-            placeholder='09/32'
-            required='required'
-            autoComplete='cc-exp'
-            value={cardPeriodValue}
-            onChange={handleCardPeriodChange}
-            maxLength='5'
-          />
-          <Input
-            id='card-code'
-            name='card-code'
-            title='CVC/CVV код'
-            placeholder='123'
-            required='required'
-            autoComplete='cc-csc'
-            value={cardCodeValue}
-            onChange={handleCardCodeChange}
-            maxLength='3'
-          />
-        </Form>
-      </div> */}
-      <div id="payment-form"></div>
-    </div>
-  )
 }
 
 export default SubscribeHasNoSubscription
